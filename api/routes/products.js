c61a045b5843e219f4c2d3e484a4006609da1d67
PsychoNarc <hridayanphukan123@gmail.com>
2022-01-15 09:29:17 +0530

    First Commit

---
+++ api/routes/products.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
+    res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    }
+    else {
+        res.status(200).json({
+            message: 'Handling GET requests to /products with params',
+            id: id
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product',
+        id: id
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product',
+        id: id
+    });
+});
+
+module.exports = router;
